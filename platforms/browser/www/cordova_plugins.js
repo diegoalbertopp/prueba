00f485a7eb9bdfa57515a6a08d71bf7b43d01a94
Diego Poblete Parra <diego.poblete1501@alumnos.ubiobio.cl>
2019-08-31 22:55:27 -0400

    Cambios importantes

--- platforms/browser/www/cordova_plugins.js
+++ platforms/browser/www/cordova_plugins.js
@@ -18,7 +18,8 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-device": "2.0.3"
+    "cordova-plugin-device": "2.0.3",
+    "cordova-plugin-whitelist": "1.3.4"
 }
 // BOTTOM OF METADATA
 });
